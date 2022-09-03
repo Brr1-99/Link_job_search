3fc25f61bf4f3ed62dc4184a1d0877cb09e7f67a
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-09-03 09:36:44 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+jobs = ["junior python", "python"]
+
+def job_url(keywords: str) -> str: 
+    keywords.replace(' ', '%20')
+    return f"https://www.linkedin.com/jobs/search/?&distance=25&f_TPR=r86400&geoId=105646813&keywords={keywords}&sortBy=DD"
+
+chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+
+def main() -> None:
+    for job in jobs:
+        webbrowser.get(chrome_path).open(job_url(job))
+
+if __name__ == "__main__":
+    main()
